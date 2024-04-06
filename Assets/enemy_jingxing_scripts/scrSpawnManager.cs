6aa699584c398247a69492e8f1541abe003f926d
botguest <119339547+botguest@users.noreply.github.com>
2024-04-06 17:20:05 -0400

    Setting up more stuff
    just that

---
+++ Assets/enemy_jingxing_scripts/scrSpawnManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//manager for controlling enemy spawn
+public class scrSpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
