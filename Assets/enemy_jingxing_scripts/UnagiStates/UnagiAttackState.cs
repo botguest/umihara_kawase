bb8d872e18bb9ba275d62ad18c147d40c1d292dc
botguest <119339547+botguest@users.noreply.github.com>
2024-04-14 21:04:43 -0400

    Unagi Set up
    The scripts for the unagi is ready

---
+++ Assets/enemy_jingxing_scripts/UnagiStates/UnagiAttackState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnagiAttackState : UnagiGroundedState
+{
+    
+    public UnagiAttackState(Enemy _enemyBase, EnemyStateMachine _stateMachine, scrEnemy_Unagi enemy) : base(_enemyBase, _stateMachine, enemy)
+    {
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/enemy_jingxing_scripts/UnagiStates/UnagiIdleState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnagiIdleState : UnagiGroundedState
+{
+
+    public UnagiIdleState(Enemy _enemyBase, EnemyStateMachine _stateMachine, scrEnemy_Unagi enemy) : base(_enemyBase,
+        _stateMachine, enemy)
+    {
+    }
+    
+    public override void Enter()
+    {
+        base.Enter();
+
+        stateTimer = enemy.idleTime;
+    }
+    
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+
+        if (stateTimer < 0)
+            stateMachine.ChangeState(enemy.moveState);
+
+
+    }
+}
