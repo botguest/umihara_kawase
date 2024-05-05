2f0765c9fccb697f56ff9eb135bd94f1b316b1ff
botguest <119339547+botguest@users.noreply.github.com>
2024-05-05 19:12:07 -0400

    Set Up for shield.
    Merged before working on Player.cs. Need latest from Eric.

---
+++ Assets/Jingxing/enemy_jingxing_scripts/Shield/scrShieldManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrShieldManager : MonoBehaviour
+{
+    public int Charge; //the charge of shield. 4 fish = 1 shield
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
